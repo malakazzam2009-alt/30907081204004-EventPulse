29407c7a0ad6bda4246f231b9570ddb11298a62b
Malak <malakazzam2009@gmail.com>
2026-08-24 21:39:21 +0300

    docs: add swagger API documentation task7

---
+++ swagger/swagger.js
@@ -0,0 +1,39 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+
+    info: {
+      title: 'EventPulse API',
+      version: '1.0.0',
+      description:
+        'Event Management Backend API — events, categories, authentication, registrations, real-time announcements.',
+    },
+
+    servers: [
+      {
+        url: 'http://localhost:5000',
+        description: 'Development Server',
+      },
+      {
+        url: 'https://event-pluse.vercel.app',
+        description: 'Production Server (Vercel)',
+      },
+    ],
+
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+
+  apis: [`${__dirname}/../routes/*.js`],
+};
+
+module.exports = swaggerJSDoc(options);
